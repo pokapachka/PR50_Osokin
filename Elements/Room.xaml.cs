9dbb594fb435d3950bc949fb0f2f6a5a25da7c94
pokapachka <pachkaosokin@yandex.ru>
2024-06-03 20:33:23 +0500

    Разработанный пользовательский элемент квартиры. Осокин

---
+++ Elements/Room.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ПР50_Осокин.Context;
+
+namespace ПР50_Осокин.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Room.xaml
+    /// </summary>
+    public partial class Room : UserControl
+    {
+        public Room(int Room)
+        {
+            InitializeComponent();
+            NameRoom.Content += Room.ToString();
+            LoadOwner(Room);
+        }
+        public void LoadOwner(int Room)
+        {
+            List<OwnerContext> roomOwners = OwnerContext.AllOwners().FindAll(x => x.NumberRoom == Room);
+            foreach (var roomOwner in roomOwners) 
+                parent.Children.Add(new Elements.Owner(roomOwner));
+        }
+    }
+}
